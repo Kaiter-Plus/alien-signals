ea7d23771b6514e336eebb05ef1b157aafef55f6
Kaiter-Plus <bundows@foxmail.com>
2024-12-26 21:50:16 +0800

    chore: split complex benchmark

--- benchs/complex.mjs
+++ benchs/complex.mjs
@@ -1,25 +1,21 @@
 import { bench, boxplot, run } from 'mitata'
 import { computed, effect, signal } from '../dist/index.mjs'
-
-global.gc ??= Bun.gc
-
 boxplot(() => {
-  bench('propagate: $w * $h', function* (state) {
+  bench('complex: $w * $h', function* (state) {
     const w = state.get('w')
     const h = state.get('h')
-    const src = signal(1)
+    const src = signal({ w, h })
     for (let i = 0; i < w; i++) {
       let last = src
       for (let j = 0; j < h; j++) {
         const prev = last
-        last = computed(() => prev.get() + 1)
+        last = computed(() => ({ [`${i}-${j}`]: prev.get() }))
       }
       effect(() => last.get())
     }
-    yield () => src.set(src.get() + 1)
+    yield () => src.set({ upstream: src.get() })
   })
     .args('h', [1, 10, 100])
     .args('w', [1, 10, 100])
 })
-
 run({ format: 'markdown' })
